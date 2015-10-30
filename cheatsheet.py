ac72b0ee3c8b5b51359856f5dde55dd96c9e3341
Seabook <seabook1024@gmail.com>
2015-10-30 16:33:50 +1100

    Update cheatsheet.py

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,1 +1,6 @@
-len(df)
+len(df)                     series + value        df[df.c == value]
+df.head()                   series + series2      df[(df.c >= value) & (df.d <  value)]
+df.tail()                   series.notnull()      df[(df.c < value) | (df.d != value)]
+df.COLUMN                   series.isnull()       df.sort('column')
+df['COLUMN']                series.order()        df.sort(['column1', 'column2'])
+
